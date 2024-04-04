2af4718dd7b47a75fc0a73d83fc2412c3c31da29
Rafał Chomczyk <hello@rafal.moe>
2024-04-04 19:27:13 +0200

    Add support for using retrieval of placeholders from package-private data objects

--- auroramc-messages-core/src/main/java/pl/auroramc/messages/placeholder/evaluator/ReflectivePlaceholderEvaluator.java
+++ auroramc-messages-core/src/main/java/pl/auroramc/messages/placeholder/evaluator/ReflectivePlaceholderEvaluator.java
@@ -1,9 +1,11 @@
 package pl.auroramc.messages.placeholder.evaluator;
 
+import static java.lang.invoke.MethodHandles.privateLookupIn;
+import static java.lang.reflect.Modifier.isPublic;
 import static pl.auroramc.messages.placeholder.evaluator.ReflectivePlaceholderEvaluatorUtils.resolveTokens;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -11,8 +13,9 @@ import java.util.Map;
 
 class ReflectivePlaceholderEvaluator implements PlaceholderEvaluator {
 
-  private static final Lookup LOOKUP = MethodHandles.lookup();
+  private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
   private static final Map<Class<?>, MethodType> METHOD_TYPE_CACHE = new HashMap<>();
+  private static final Map<Class<?>, MethodHandles.Lookup> LOOKUP_CACHE = new HashMap<>();
   private static final Map<PlaceholderCompositeKey, Class<?>> RETURN_TYPE_CACHE = new HashMap<>();
 
   ReflectivePlaceholderEvaluator() {}
@@ -24,9 +27,8 @@ class ReflectivePlaceholderEvaluator implements PlaceholderEvaluator {
 
   @Override
   public Object evaluate(final Object object, final String path, final Class<?> type) {
-    final MethodType methodType = METHOD_TYPE_CACHE.computeIfAbsent(type, MethodType::methodType);
     try {
-      return LOOKUP.findVirtual(object.getClass(), resolveTokens(path), methodType).invoke(object);
+      return getMethodHandle(object, path, type).invoke(object);
     } catch (final Throwable exception) {
       throw new PlaceholderEvaluationException(
           "Could not evaluate placeholders, because of unexpected exception.", exception);
@@ -38,6 +40,26 @@ class ReflectivePlaceholderEvaluator implements PlaceholderEvaluator {
     return RETURN_TYPE_CACHE.computeIfAbsent(
         new PlaceholderCompositeKey(object.getClass(), path),
         key -> getReturnType0(object, resolveTokens(path)));
+  }
+
+  private MethodHandle getMethodHandle(
+      final Object object, final String path, final Class<?> type) {
+    final MethodType methodType = METHOD_TYPE_CACHE.computeIfAbsent(type, MethodType::methodType);
+    try {
+      final Class<?> clazz = object.getClass();
+      if (LOOKUP_CACHE.containsKey(clazz)) {
+        return LOOKUP_CACHE.get(clazz).findVirtual(clazz, resolveTokens(path), methodType);
+      }
+
+      final MethodHandles.Lookup lookup =
+          isPublic(clazz.getModifiers()) ? LOOKUP : privateLookupIn(clazz, LOOKUP);
+      LOOKUP_CACHE.put(clazz, lookup);
+      return lookup.findVirtual(clazz, resolveTokens(path), methodType);
+    } catch (final NoSuchMethodException | IllegalAccessException exception) {
+      throw new PlaceholderEvaluationException(
+          "Could not evaluate placeholders, because of failed retrieval of method handle.",
+          exception);
+    }
   }
 
   private Class<?> getReturnType0(final Object object, final String path) {
